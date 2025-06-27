d1c43052d1a3e8ced3445cf0201d2e37b23dd07a
Nicholas McKee <nmsmckee@gmail.com>
2025-06-26 21:37:37 -0400

    Fix duplicate ids for checkbox

--- src/components/charts/MultiLabelSwitch.vue
+++ src/components/charts/MultiLabelSwitch.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { Switch } from "../ui/switch";
 import { Label } from "../ui/label";
+import { useId } from "vue";
 
 const { left, right } = defineProps<{ left: string; right: string }>();
 const model = defineModel<boolean>();
+
+const inputId = useId();
 </script>
 
 <template>
   <div class="ms-8 mt-2 flex items-center">
-    <Label for="isSingle">{{ left }}</Label>
-    <Switch v-model:checked="model" id="isSingle" class="mx-3" />
-    <Label for="isSingle">{{ right }}</Label>
+    <Label :for="inputId">{{ left }}</Label>
+    <Switch v-model:checked="model" :id="inputId" class="mx-3" />
+    <Label :for="inputId">{{ right }}</Label>
   </div>
 </template>
